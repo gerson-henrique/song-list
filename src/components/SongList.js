c8b248a45e1953ed3770ace1da41fdd5d8d04d1c
Gero <gersonhoa@gmail.com>
2022-02-24 15:46:59 -0300

    init : cria a pasta components e a base do song-list

---
+++ src/components/SongList.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const SongList = () => {
+  return(
+    <div className='song-list'>
+      <ul>
+        <li>Despair hangover e ecstasy</li>
+        <li>Genesis</li>
+        
+      </ul>
+    </div>
+  )
+}
+
+export default SongList;
